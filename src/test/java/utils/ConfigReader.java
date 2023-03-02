0ec7445b25c6b5321612b130fe98936939f439a4
Sancho Krupnikov <krupnikov@gmail.com>
2023-03-02 21:32:46 +0300

    Add project

---
+++ src/test/java/utils/ConfigReader.java
@@ -0,0 +1,23 @@
+package utils;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ConfigReader {
+
+    public static String getConfig (String key) {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = null;
+        try (FileReader reader = new FileReader("src/test/resources/config.json")) {
+            jsonObject = (JSONObject) parser.parse(reader);
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return key = (String) jsonObject.get(key);
+    }
+
+}
